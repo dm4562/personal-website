885ad8bf7cc1b7e72afbaf64574e7c22ef3eb7bb
Dhruv Mehra <dmehra6@gatech.edu>
2016-09-06 01:53:32 -0400

    get typing to work

--- js/app.js
+++ js/app.js
@@ -9,11 +9,27 @@ $(document).ready(function() {
       ],
       typeSpeed: 100,
       startDelay: 500,
-      resetCallback: setTimeout(setup, 23000)
+      callback: setTimeout(setup, 21000)
     });
   });
 });
 
 var setup = function() {
-  alert("Boom");
-}
+  var font = parseInt($(".text").css('font-size'));
+  console.log(font, font * 1.3);
+  font = font * 1.3;
+  console.log("2", font);
+  // $(".text").animate({
+  //   fontSize: font + 'px'
+  // }, {
+  //   duration: 500,
+  //   queue: false
+  // });
+  // $(".typed-cursor").animate({
+  //   fontSize: font + 'px'
+  // }, {
+  //   duration: 500,
+  //   queue: false
+  // });
+  // $(".navbar").slideDown("slow");
+};
